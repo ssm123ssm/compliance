5e59b039e70b989e46e64f323e9733e628eac3dc
Supun Manathunga <ssm123ssm@gmail.com>
2024-03-10 08:46:07 +0530

    disabling auto zoom on mobile

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,12 +1,31 @@
+import Head from "next/head";
+
 export const metadata = {
   title: "Compliance Dashboard",
   description: "Neurasense",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 import React from "react";
 
 const layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      {children}
+    </div>
+  );
 };
 
 export default layout;
--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth/next";
 import { Inter } from "next/font/google";
 import Provider from "./components/Provider";
 import { Themes } from "./providers";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -16,10 +17,6 @@ export default async function RootLayout({ children }) {
   console.log(session);
   return (
     <html lang="en" className="dark text-foreground bg-background">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-      />
       <body className="">
         <Provider session={session}>
           <Themes>{children}</Themes>
